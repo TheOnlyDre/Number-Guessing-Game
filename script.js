4836e0ffc17378af46ac65d182eca9d80a5a03d1
Andre <138155444+TheOnlyDre@users.noreply.github.com>
2024-04-27 13:36:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Declare variables
+let attempts = 0;
+let randomNum = Math.floor(Math.random() * 100 + 1);
+
+const guess= document.getElementById("guess");
+const submit = document.getElementById("submit");
+const hint = document.getElementById("hint");
+const attemptsText = document.getElementById("attempts");
+
+// Event listeners
+submit.addEventListener("click", checkGuess);
+
+function checkGuess() {
+    const userValue = Number(guess.value);
+    attempts++;
+
+    // Conditionals
+    if(userValue === randomNum) {
+        hint.textContent = "Congratulations, you guessed it!";
+    } else if (userValue < randomNum) {
+        hint.textContent = "Too low! Try again"
+    } else {
+        hint.textContent = "Too high! Try again";
+    }
+
+    attemptsText.textContent = "Attempts: " + attempts;
+}
